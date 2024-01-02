b80aa53e9b0d86a43ff7df209b27c9913e2be5bc
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2024-01-02 04:42:16 -0500

    Fix counter command issue

--- bot.js
+++ bot.js
@@ -35,41 +35,40 @@ client.on("ready", () => {
 client.on("message", async message => {
     if (message.author.bot) return;
     
-    if(regex.test(message.content.toLowerCase()) && !message.content.startsWith('!userbamfcount')){
-     const username = message.author.username;
-     if (!data.users[username]) {
-         data.users[username] = 0;
-     }
-     data.users[username] += 1;
-     data.count += 1;
-     var writestring = JSON.stringify(data);
-     fs.writeFileSync("data/data.json", writestring);
+    if(regex.test(message.content.toLowerCase()) && !message.content.startsWith('!userbamfcount') && !message.content.startsWith('!bamfcount')){
+    const username = message.author.username;
+    if (!data.users[username]) {
+        data.users[username] = 0;
+    }
+    data.users[username] += 1;
+    data.count += 1;
+    var writestring = JSON.stringify(data);
+    fs.writeFileSync("data/data.json", writestring);
     }
     
     if (message.content.startsWith('!bamfcount')) {
-        
-        let totalCount = 0;
-        for (let user in data.users) {
+    let totalCount = 0;
+    for (let user in data.users) {
         totalCount += data.users[user];
-        }
-        message.reply(`||${searchBamf[0]}|| has been said ${totalCount} times by all users.`);
+    }
+    message.reply(`||${searchBamf[0]}|| has been said ${totalCount} times by all users.`);
     }
     
     if (message.content.startsWith('!userbamfcount')) {
-     const args = message.content.split(' ');
-     
-     let username;
-     if (args.length > 1) {
-         username = args[1];
-     } else {
-         username = message.author.username;
-     }
-     
-     if (!data.users[username]) {
-         message.reply(`User ${username} has not said the word yet.`);
-     } else {
-         message.reply(`User ${username} has said ||${searchBamf[0]}|| ${data.users[username]} times.`);
-     }
+    const args = message.content.split(' ');
+    
+    let username;
+    if (args.length > 1) {
+        username = args[1];
+    } else {
+        username = message.author.username;
+    }
+    
+    if (!data.users[username]) {
+        message.reply(`User ${username} has not said the word yet.`);
+    } else {
+        message.reply(`User ${username} has said ||${searchBamf[0]}|| ${data.users[username]} times.`);
+    }
     }
    });
    
